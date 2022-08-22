48bfe70c04eb5c53bdf2ab88183c3a3bb1b940d0
Ruth Jepkalal <kipchauijepkaa@gmail.com>
2022-08-22 01:46:39 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function addingEventListener() {
+
     const input=document.getElementById("button");
+
     input.addEventListener("click",function(){
+
         alert("clickAlert")
     })
 }
+addingEventListener();
